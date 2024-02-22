bd10aa96809d8816c0b64af57ca0a4d19b115d26
itsobby <pascalobala@gmail.com>
2024-02-22 16:13:28 +0300

    Implimenting a basic custom user model manager

---
+++ myVenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\odongo\Documents\django_rest_api_project\myVenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
